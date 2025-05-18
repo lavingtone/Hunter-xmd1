c64624a818fe775f0d5dbbb840a316622c1cb8ab
Obed TechX <obedomoke360@gmail.com>
2025-05-18 06:16:13 +0300

    Update news.js

--- plugins/news.js
+++ plugins/news.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, reply }) => {
 ⚠️ _${article.description}_
 🔗 _${article.url}_
 
-  ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ Jᴀᴡᴀᴅ TᴇᴄʜX
+  ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ Obed TechX
             `;
 
             console.log('Article URL:', article.urlToImage); // Log image URL for debugging
